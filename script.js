d0801e28cc14bb1d7af636617c73e77e0aa0d1dd
Juan <4684593@gmail.com>
2025-04-22 21:19:36 -0500

    Mi cv

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-document.addEventListener("DOMContentLoaded", function(){
-
+document.addEventListener("DOMContentLoaded", function () {
     const downloadBtn = document.getElementById("downloadBtn");
-
-
-    if(downloadBtn){ 
-        downloadBtn.addEventListener("click", function(){
-            const elemen = document.body;
-
-            const content = {
-                margin: 0.5,
-                filename: "CV.pdf",
-                image: {type: "jpeg",quality: 0.98},
-                html2canvas: { scale: 2 },
-                jsPDF: { unit: "in", format: "letter", orientation: "portrait"}
-            };
-
-            html2pdf().set(content).from(elemen).save();
-    });
-
-    }
-});
+  
+    if (downloadBtn) {
+      downloadBtn.addEventListener("click", function () {
+        const element = document.body;
+  
+        const opt = {
+          margin: 0.5,
+          filename: "cv.pdf",
+          image: { type: "jpeg", quality: 0.98 },
+          html2canvas: { scale: 2 },
+          jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
+        };
+  
+        html2pdf().set(opt).from(element).save();
+      });
+    }
+  });
